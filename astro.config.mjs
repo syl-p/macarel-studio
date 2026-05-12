90b801dc3c4ba67e823a470cdc95987c95f461cd
Sylvain <sylvain.pastor@top-webgroup.com>
2026-05-12 10:35:37 +0200

    fix: change domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.macarel.studio',
+  site: 'https://www.websylvain.com',
   vite: {
     plugins: [tailwindcss()]
   }
